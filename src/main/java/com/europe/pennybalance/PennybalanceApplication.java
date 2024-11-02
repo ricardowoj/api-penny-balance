f5ac8ba5a23bc83f5f228a99aab5379377ff8ae7
Ricardo <ricardowoj@hotmail.com>
2024-11-02 16:47:32 +0000

    add first commit

---
+++ src/main/java/com/europe/pennybalance/PennybalanceApplication.java
@@ -0,0 +1,13 @@
+package com.europe.pennybalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennybalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennybalanceApplication.class, args);
+	}
+
+}
